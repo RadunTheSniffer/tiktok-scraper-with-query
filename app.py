77dc6db3e58b4761340ffb872d4491f83cc318b6
RadunTheSniffer <ridhuan.work2001@gmail.com>
2024-09-11 10:35:48 +0800

    Update app.py
    Results.html redirect

--- app.py
+++ app.py
@@ -1,41 +1,32 @@
 from flask import Flask, request, jsonify, render_template
-import tweepy
 import snscrape.modules.twitter as sntwitter
+from datetime import datetime
 
 app = Flask(__name__)
-
-# Tweepy credentials
-api_key = 'YOUR_API_KEY'
-api_secret_key = 'YOUR_API_SECRET_KEY'
-access_token = 'YOUR_ACCESS_TOKEN'
-access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
-
-# Authenticate to Twitter
-auth = tweepy.OAuth1UserHandler(api_key, api_secret_key, access_token, access_token_secret)
-api = tweepy.API(auth)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/scrape_tweepy', methods=['POST'])
-def scrape_tweepy():
-    username = request.form['username']
-    count = int(request.form['count'])
-    tweets = api.user_timeline(screen_name=username, count=count)
-    tweet_data = [{'username': tweet.user.screen_name, 'date': tweet.created_at, 'text': tweet.text} for tweet in tweets]
-    return jsonify(tweet_data)
-
 @app.route('/scrape_snscrape', methods=['POST'])
 def scrape_snscrape():
-    username = request.form['username']
+    keyword = request.form['keyword']
     count = int(request.form['count'])
     tweets = []
-    for tweet in sntwitter.TwitterUserScraper(username).get_items():
+    for tweet in sntwitter.TwitterSearchScraper(f'{keyword}').get_items():
         if len(tweets) == count:
             break
-        tweets.append({'username': tweet.user.username, 'date': tweet.date, 'text': tweet.content})
-    return jsonify(tweets)
+        tweets.append({
+            'username': tweet.user.username,
+            'date': tweet.date.strftime('%Y-%m-%d %H:%M:%S'),
+            'text': tweet.content
+        })
+    
+    # Sort tweets by date
+    tweets.sort(key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d %H:%M:%S'), reverse=True)
+    
+    return render_template('results.html', tweets=tweets)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
