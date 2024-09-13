313c45338bee82bdc13337d3c2012dea28ad8e72
RadunTheSniffer <ridhuan.work2001@gmail.com>
2024-09-13 18:15:26 +0800

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import snscrape.modules.twitter as sntwitter
 from datetime import datetime
+import time
 
 app = Flask(__name__)
 
@@ -10,21 +11,25 @@ def index():
 
 @app.route('/scrape_snscrape', methods=['POST'])
 def scrape_snscrape():
-    keyword = request.form['keyword']
-    count = int(request.form['count'])
+    username = request.form.get('username')
+    count = request.form.get('count')
+    if not username or not count:
+        return "Missing username or count", 400
+    count = int(count)
     tweets = []
-    for tweet in sntwitter.TwitterSearchScraper(f'{keyword}').get_items():
-        if len(tweets) == count:
-            break
-        tweets.append({
-            'username': tweet.user.username,
-            'date': tweet.date.strftime('%Y-%m-%d %H:%M:%S'),
-            'text': tweet.content
-        })
-    
-    # Sort tweets by date
+    try:
+        for tweet in sntwitter.TwitterSearchScraper(f'from:{username}').get_items():
+            if len(tweets) == count:
+                break
+            tweets.append({
+                'username': tweet.user.username,
+                'date': tweet.date.strftime('%Y-%m-%d %H:%M:%S'),
+                'text': tweet.content
+            })
+            time.sleep(1)  # Add a 1-second delay between requests
+    except sntwitter.ScraperException as e:
+        return f"An error occurred: {e}", 500
     tweets.sort(key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d %H:%M:%S'), reverse=True)
-    
     return render_template('results.html', tweets=tweets)
 
 if __name__ == '__main__':
